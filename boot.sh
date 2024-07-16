51de3f4c9015be4ffc0d82458dd9ad02e0eb4217
youtipie18 <youtipie@gmail.com>
2024-07-16 16:54:35 +0300

    Add ability to skip scraping all movies and series

--- boot.sh
+++ boot.sh
@@ -4,8 +4,13 @@ chmod 0644 /etc/cron.d/crontab
 
 touch /var/log/cron.log
 
-./start_once_spiders.sh
+if [ -n "$INITIAL_SCRAPING" ] && [ "$INITIAL_SCRAPING" -eq 1 ]; then
+  ./start_once_spiders.sh
+else
+   echo "Skipping initial scraping."
+fi
 
 ./run_cron.sh
+
 
 cron && tail -f /var/log/cron.log
